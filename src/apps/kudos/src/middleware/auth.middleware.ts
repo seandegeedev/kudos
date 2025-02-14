4e0e8fd4a278b1d8a1845d0da3660456db4851bb
Sean De Gee <dev@seandegee.tech>
2025-02-14 21:02:14 +0200

    refactor

--- src/apps/kudos/src/middleware/auth.middleware.ts
+++ src/apps/kudos/src/middleware/auth.middleware.ts
@@ -1,7 +1,7 @@
 import { NextRequest } from 'next/server';
 
 export const isGetStartedRequired = async (req: NextRequest): Promise<boolean> => {
-  return true;
+  return false;
 };
 
 export const isLoggedIn = (req: NextRequest) => {};
