2f379a794a65bd758642b4ee4030565e0a6495d5
Sean De Gee <dev@seandegee.tech>
2025-02-15 07:40:14 +0200

    refactor

--- src/apps/kudos/src/api/index.ts
+++ src/apps/kudos/src/api/index.ts
@@ -1,6 +1,7 @@
 const BASE_URL = process.env.API_SERVER_URL;
 const DEFAULT_OPTIONS: RequestInit = {};
 
+/* Wrapper around fetch that adds the base URL and default options */
 const apiFetch = async (relativePath: string | URL | Request, options?: RequestInit): Promise<Response> => {
   if (typeof relativePath === 'string' && relativePath.startsWith('/')) {
     relativePath = relativePath.slice(1);
