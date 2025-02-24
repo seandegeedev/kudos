bf5baf8844fc1f7a184db7f873b6da172f0c9dd5
Sean De Gee <dev@seandegee.tech>
2025-02-24 17:04:42 +0200

    version header

---
+++ src/apps/kudos/components/App/VersionHeader.vue
@@ -0,0 +1,24 @@
+<script lang="ts" setup>
+  import { version } from '@/package.json';
+</script>
+
+<template>
+  <header class="app-version-header">
+    <div>
+      <span class="version-text">Kudos Release {{ version }}</span>
+    </div>
+  </header>
+</template>
+
+<style lang="scss" scoped>
+  .app-version-header {
+    padding: 0.5rem 1rem;
+
+    display: flex;
+    justify-content: flex-end;
+  }
+
+  .version-text {
+    color: var(--color-text-muted);
+  }
+</style>
