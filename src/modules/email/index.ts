8bd539f428bb6b483d4c2b17176705eaf6aba88d
Sean De Gee <dev@seandegee.tech>
2025-05-10 09:40:53 +0200

    changes

--- src/modules/email/index.ts
+++ src/modules/email/index.ts
@@ -1,6 +1,31 @@
+import pRetry from 'p-retry';
 import transporter, { SMTP_USER } from './transporter';
+import type { Options as RetryOptions } from 'p-retry';
 
 const CLIENT_URL = process.env.API_CLIENT_URL || '';
+
+const MAX_RETRIES = 3;
+const RETRY_DELAY = 1000; // 1 second
+
+const RETRY_OPTIONS: RetryOptions = {
+  retries: MAX_RETRIES,
+  minTimeout: RETRY_DELAY,
+};
+
+// Send email with retry logic
+const sendMail = async (mailOptions: any) => {
+  return pRetry(
+    async () => {
+      await transporter.sendMail(mailOptions);
+    },
+    {
+      ...RETRY_OPTIONS,
+      onFailedAttempt: error => {
+        console.error(`Attempt ${error.attemptNumber} failed. Retrying...`);
+      },
+    }
+  );
+};
 
 export const sendVerificationEmail = async ({
   firstName,
@@ -18,7 +43,7 @@ export const sendVerificationEmail = async ({
     text: `Hello, ${firstName}\n\nWelcome to Kudos 💪\nEnter the code below to verify your account:\n\n${code}\n\nIf you did not request this, please ignore this email.`,
   };
 
-  await transporter.sendMail(mailOptions);
+  await sendMail(mailOptions);
 };
 
 export const sendPasswordResetEmail = async ({
@@ -37,7 +62,7 @@ export const sendPasswordResetEmail = async ({
     text: `Hello, ${firstName}\n\nClick the link below to reset your password:\n\n${CLIENT_URL}/reset-password?token=${token}\n\nIf you did not request this, please ignore this email.`,
   };
 
-  await transporter.sendMail(mailOptions);
+  await sendMail(mailOptions);
 };
 
 export default {
