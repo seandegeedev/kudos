bd0b2e912f8cdb2963f13d4277a2b7b9f65d000a
Sean De Gee <dev@seandegee.tech>
2025-05-10 23:02:03 +0200

    bun update

--- util/server_setup_dev.sh
+++ util/server_setup_dev.sh
@@ -26,7 +26,7 @@ sudo apt install ca-certificates curl unzip -y
 #  Bun Installation
 # ============================================
 
-# Download and install latest version of Bun. For a specific version, replace with [curl -fsSL https://bun.sh/install | bash -s "bun-v1.2.7"]
+# Download and install latest version of Bun. For a specific version, replace with [curl -fsSL https://bun.sh/install | bash -s "bun-v1.2.13"]
 curl -fsSL https://bun.sh/install | bash
 
 # Make bun useable from the current shell
