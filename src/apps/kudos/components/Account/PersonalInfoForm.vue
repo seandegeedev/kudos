bceec6e1e0f68732ca9aa4e90cd957cfb4abe59e
Sean De Gee <dev@seandegee.tech>
2025-05-10 10:33:36 +0200

    changes

--- src/apps/kudos/components/Account/PersonalInfoForm.vue
+++ src/apps/kudos/components/Account/PersonalInfoForm.vue
@@ -1,7 +1,14 @@
 <script lang="ts" setup></script>
 
 <template>
-  <form action.prevent><AccountAvatarChanger /></form>
+  <form action.prevent class="personal-info-form"><AccountAvatarChanger /></form>
 </template>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+  .personal-info-form {
+    padding: 1rem 0;
+
+    display: grid;
+    gap: 1rem;
+  }
+</style>
