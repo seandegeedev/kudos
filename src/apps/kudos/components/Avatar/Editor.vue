4065ce26c1bd9acd8614df33859c2e7be3c87c6a
Sean De Gee <dev@seandegee.tech>
2025-05-18 09:04:41 +0200

    changes

--- src/apps/kudos/components/Avatar/Editor.vue
+++ src/apps/kudos/components/Avatar/Editor.vue
@@ -79,6 +79,7 @@
   };
 
   const verifyClose = () => {
+    cancelImageSelection();
     popup.value?.close();
   };
 
@@ -337,5 +338,18 @@
 
   .save-button {
     justify-content: center;
+
+    animation: slide-up 0.3s ease-in-out forwards;
+
+    @keyframes slide-up {
+      0% {
+        opacity: 0;
+        transform: translateY(1rem);
+      }
+      100% {
+        opacity: 1;
+        transform: translateY(0);
+      }
+    }
   }
 </style>
