0c3463ffd45b1ce6becade9d69c80eb338c1f1de
Sean De Gee <dev@seandegee.tech>
2025-05-19 13:16:39 +0200

    changes

---
+++ src/apps/kudos/components/Admin/Users/InvitationView.vue
@@ -0,0 +1,14 @@
+<script lang="ts" setup>
+  defineProps({
+    invitationID: {
+      type: String,
+      required: true,
+    },
+  });
+</script>
+
+<template>
+  <div class="invitation-view">
+    <span>Invitation View</span>
+  </div>
+</template>
--- src/apps/kudos/components/Admin/Users/NewInvite.vue
+++ src/apps/kudos/components/Admin/Users/NewInvite.vue
@@ -6,12 +6,30 @@
 
   const popup = ref<InstanceType<typeof AppPopup> | null>(null);
 
+  const email = ref<string>('');
+  const invitationID = ref<string>('');
+
+  const invitationCreated = computed(() => {
+    return invitationID.value !== '';
+  });
+
   const open = () => {
     popup.value?.open();
   };
 
-  const verifyClose = () => {
+  const close = () => {
+    invitationID.value = '';
+    email.value = '';
+
     popup.value?.close();
+  };
+
+  const getInviteCode = async () => {
+    invitationID.value = '12345678'; // Simulate an API call to get the invite code
+    messageQueueStore.addMessage({
+      type: 'success',
+      message: `Invite code ${invitationID.value} created successfully!`,
+    });
   };
 
   defineExpose({
@@ -20,14 +38,27 @@
 </script>
 
 <template>
-  <AppPopup ref="popup" @click-outside="verifyClose">
+  <AppPopup ref="popup" @click-outside="close">
     <div class="new-invite">
       <header class="header">
         <h2 class="header-title">Invite to Kudos</h2>
-        <FormButton class="header-close" type="outline" size="small" @click="verifyClose">
-          <span>Cancel</span>
+        <FormButton class="header-close" type="outline" size="small" @click="close">
+          <span>{{ invitationCreated ? 'Close' : 'Cancel' }}</span>
         </FormButton>
       </header>
+      <template v-if="invitationCreated">
+        <AdminUsersInvitationView :invitationID="invitationID" />
+      </template>
+      <template v-else>
+        <form @submit.prevent>
+          <FormFieldText label="Email" name="email" placeholder="Enter email" />
+        </form>
+        <div class="actions">
+          <FormButton @click="getInviteCode">
+            <span>Get Invite Code</span>
+          </FormButton>
+        </div>
+      </template>
     </div>
   </AppPopup>
 </template>
@@ -47,12 +78,21 @@
   }
 
   .header {
-    align-items: center;
+    padding-bottom: 0.5rem;
+
+    align-items: flex-end;
     display: flex;
     justify-content: space-between;
+
+    border-bottom: 1px solid var(--color-border-00);
   }
 
   .header-title {
     font-size: 1.2rem;
+  }
+
+  .actions {
+    display: flex;
+    justify-content: flex-end;
   }
 </style>
