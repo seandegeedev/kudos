0ca4058a45b23c8a113b94ae06cbd5940fe59712
Sean De Gee <dev@seandegee.tech>
2025-05-19 06:51:00 +0200

    changes

--- src/apps/kudos/pages/app/admin.vue
+++ src/apps/kudos/pages/app/admin.vue
@@ -2,7 +2,10 @@
   <div class="admin page-basic">
     <header class="header">
       <h1 class="header-title">Kudos Admin</h1>
-      <div class="admin-tabs"></div>
+      <div class="admin-tabs">
+        <div class="admin-tab admin-tab__selected">Users</div>
+        <div class="admin-tab">Realms</div>
+      </div>
     </header>
     <main class="main">
       <NuxtPage />
@@ -11,11 +14,36 @@
 </template>
 
 <style lang="scss" scoped>
+  .header {
+    align-items: center;
+    gap: 1rem;
+    display: grid;
+    grid-template-columns: auto 1fr;
+  }
+
   .header-title {
     font-size: 1.4rem;
   }
 
   .admin-tabs {
+    padding: 0.25rem;
+
+    display: flex;
+    gap: 0.25rem;
+
+    background-color: var(--color-background-01);
+    border-radius: 0.5rem;
+    cursor: pointer;
+  }
+
+  .admin-tab {
+    padding: 0.25rem 0.5rem;
+
+    border-radius: 0.3rem;
+
+    &.admin-tab__selected {
+      background-color: var(--color-background-00);
+    }
   }
 
   .main {
